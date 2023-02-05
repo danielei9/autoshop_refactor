fc71c5efe717af1d588388fb16347b682b3f8669
danielei9 <dbs99@hotmail.com>
2023-02-05 21:44:17 +0100

    Update CoinWalletController.py

--- Controller/CoinWallet/CoinWalletController.py
+++ Controller/CoinWallet/CoinWalletController.py
@@ -266,7 +266,7 @@ class CoinWalletController(SerialCommunicator):
         while True:
             if(self.com.in_waiting):    
                 try:
-                    received =  self.com.readline().decode('utf-8')
+                    received =  self.com.readline().decode('unicode_escape')
                 except serial.SerialException:
                     print('Port is not available')
                     return False
