f12c29a7ae6caca76508aa6fa8bb12d33d27cf12
danielei9 <dbs99@hotmail.com>
2023-02-05 13:17:58 +0100

    Update BillWalletInit.py

--- Controller/BillWallet/BillWalletInit.py
+++ Controller/BillWallet/BillWalletInit.py
@@ -15,24 +15,27 @@ def main(com,cb):
         print("startPollThread")
         asyncio.run( bv.poll())   
     # port = '/dev/ttyUSB0'  # JCM UAC device (USB serial adapter)
-
-    bv = BillVal(com,cb)
-    print("Please connect bill validator.")
-    bv.power_on()
-    pollThread = threading.Thread(target=startPollThread)
-    pollThread.start()
-    if bv.init_status == id003.POW_UP:
-        logging.info("BV powered up normally.")
-    elif bv.init_status == id003.POW_UP_BIA:
-        logging.info("BV powered up with bill in acceptor.")
-    elif bv.init_status == id003.POW_UP_BIS:
-        logging.info("BV powered up with bill in stacker.")
-    bv.set_inhibit(0)
-    time.sleep(5)
-    bv.set_inhibit(1)
-    time.sleep(5)
-    bv.set_inhibit(0)
-   
+    try:
+        bv = BillVal(com,cb)
+        print("Please connect bill validator.")
+        bv.power_on()
+        pollThread = threading.Thread(target=startPollThread)
+        pollThread.start()
+        if bv.init_status == id003.POW_UP:
+            logging.info("BV powered up normally.")
+        elif bv.init_status == id003.POW_UP_BIA:
+            logging.info("BV powered up with bill in acceptor.")
+        elif bv.init_status == id003.POW_UP_BIS:
+            logging.info("BV powered up with bill in stacker.")
+        bv.set_inhibit(0)
+        time.sleep(5)
+        bv.set_inhibit(1)
+        time.sleep(5)
+        bv.set_inhibit(0)
+    except Exception as e:
+        print(e)
+        pass
+    
 
 
 # if __name__ == '__main__':
