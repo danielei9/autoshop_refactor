fc6745fc44fdf843f43fd302f294994366878c7c
danielei9 <dbs99@hotmail.com>
2023-02-05 12:59:32 +0100

    Update BillWalletInit.py

--- Controller/BillWallet/BillWalletInit.py
+++ Controller/BillWallet/BillWalletInit.py
@@ -21,7 +21,7 @@ def main(com,cb):
         logging.info("BV powered up with bill in acceptor.")
     elif bv.init_status == id003.POW_UP_BIS:
         logging.info("BV powered up with bill in stacker.")
-    bv.set_inhibit(0)
+    bv.set_inhibit(1)
     bv.poll()
 
 
