bc0f6a592966c19b984fa6d76843ccfc397e3a5c
danielei9 <dbs99@hotmail.com>
2023-02-06 18:07:13 +0100

    Update PaymentService.py

--- Services/PaymentService.py
+++ Services/PaymentService.py
@@ -224,7 +224,7 @@ class PaymentService():
         self.paymentDone = False
         self.priceClientShouldPay = payRequest.price
         self.coinWalletService.coinwallet.enableInsertCoins()
-        self.billWalletService.bv.set_inhibit(1)
+        # self.billWalletService.bv.set_inhibit(1)
         # Esperando a pagar
         while self.paymentDone == False:
             print("waiting pay")
