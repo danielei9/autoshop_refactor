d8052d04aa0fb89402ccf76c9efdee3e8dcc44c1
danielei9 <dbs99@hotmail.com>
2023-02-10 19:16:31 +0100

    Update BillWallet.py

--- Controller/BillWallet/BillWallet.py
+++ Controller/BillWallet/BillWallet.py
@@ -174,7 +174,7 @@ class BillVal:
         self.com.flushInput()
         self.com.flushOutput()
         time.sleep(0.2)
-        message = bytes([0xFC ,0x0D, 0xF0, 0x20, 0xD0, stack1, 0x00, 0x01, stack2,0x00,0x02])
+        message = bytes([0xFC ,0x0D, 0xF0, 0x20, 0xD0, confByteStack1, 0x00, 0x01, confByteStack2,0x00,0x02])
         message += get_crc(message)
 
         self.com.write(message)
