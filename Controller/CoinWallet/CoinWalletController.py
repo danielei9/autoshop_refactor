bfaaab6d516f6a02848d17a6ddb3f0f429938b93
danielei9 <dbs99@hotmail.com>
2023-03-07 19:40:58 +0100

    Update CoinWalletController.py

--- Controller/CoinWallet/CoinWalletController.py
+++ Controller/CoinWallet/CoinWalletController.py
@@ -208,8 +208,8 @@ class CoinWalletController(SerialCommunicator):
                 AssertionError("Error reading tube status")
             print("TUBE STATUS: ",response)
             self.tubeFullState = []
-            tubeByte1 = str(response[0])
-            tubeByte2 = str(response[1])
+            tubeByte1 = int(response[0].hex(), 16) 
+            tubeByte2 = int(response[1].hex(), 16) 
 
             print("Full tube Byte1:  ",tubeByte1)
             print("Full tube Byte2:  ",tubeByte2)
@@ -218,12 +218,12 @@ class CoinWalletController(SerialCommunicator):
             self.getIfTubeIsFull(tubeByte1)
             self.getIfTubeIsFull(tubeByte2)
 
-            self.tubeQnty_0_05 = int(response[2],16)
-            self.tubeQnty_0_10 = int(response[3],16)
-            self.tubeQnty_0_20 = int(response[4],16)
-            self.tubeQnty_0_50 = int(response[5],16)
-            self.tubeQnty_1_00 = int(response[6],16)
-            self.tubeQnty_2_00 = int(response[7],16)
+            self.tubeQnty_0_05 = int(response[2].hex(), 16) 
+            self.tubeQnty_0_10 = int(response[3].hex(), 16) 
+            self.tubeQnty_0_20 = int(response[4].hex(), 16) 
+            self.tubeQnty_0_50 = int(response[5].hex(), 16) 
+            self.tubeQnty_1_00 = int(response[6].hex(), 16) 
+            self.tubeQnty_2_00 = int(response[7].hex(), 16) 
 
             print(" tubeQnty_0_05:  ",self.tubeQnty_0_05)
             print(" tubeQnty_0_10:  ",self.tubeQnty_0_10)
