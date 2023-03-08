4961b8090de3a140314166cff8f1f5fbd1744846
danielei9 <dbs99@hotmail.com>
2023-03-08 16:22:20 +0100

    Clean Coinwallet

--- Controller/CoinWallet/CoinWalletController.py
+++ Controller/CoinWallet/CoinWalletController.py
@@ -29,17 +29,11 @@ from Controller.CoinWallet.CodesCoinWallet import *
 from Controller.SerialCommunicator import *
 
 class CoinWalletController(SerialCommunicator):
-    statusDeactiveThread = False
     incommingCoin = ''
     status = ''
     data = ''
-    # initialize the  connection to the com port
-    """-------------------------- COINWALLET() ------------------------------"""
-    """-------------------------- PRIVATE FUNCTIONS ------------------------------"""
-    """-------------------------- Constructor ------------------------------"""
+
     def __init__(self, cb,port):
-#         self.proc = multiprocessing.Process(target=self.threadReceived, args=())
-#         self.proc.daemon = True
         super().__init__(port,parity=serial.PARITY_NONE)
         self.cw_events = {
             DOS_EURO: self.__onInserted2Euro,
@@ -76,8 +70,6 @@ class CoinWalletController(SerialCommunicator):
         time.sleep(0.4)
         self.tubeStatus()
 
-    """-------------------------- EVENTS ------------------------------"""
-#     REVISAR LOS PARAMETROS DEL CALLBACK cb Ya Que no se usa la funcion cashbackroutine
     def __onInserted05Cent(self,data):
             print("0.05 euro")
             self.cb(0.05)
@@ -96,8 +88,6 @@ class CoinWalletController(SerialCommunicator):
     def __onInserted2Euro(self,data):
         print("2 euros")
         self.cb(2)
-    # # # # # # # # # # # # # # # # # # # # # # 
-    """--------------------------send command ------------------------------"""
     def __sendCommand(self,command):
         try:
             print("sendCommand: " , command)
@@ -108,7 +98,6 @@ class CoinWalletController(SerialCommunicator):
             pass
 
         return response
-    """------------------ send command and receive --------------------------"""
     def __sendCommandAndReceive(self,command):
         try:
             self.com.write(bytearray(command))
@@ -128,8 +117,6 @@ class CoinWalletController(SerialCommunicator):
             # TODO: Informar al usuario que no se ha podido enviar el comando 
             print("Error sending")
             pass
-            
-    """---------------------------- ParseBytes ------------------------------"""
     def __parseBytes(self,received):
         status = str(received)[2:4]
         data = str(received)[5:(len(str(received))-5)]
@@ -143,17 +130,20 @@ class CoinWalletController(SerialCommunicator):
             print("esta en el event")
             self.cw_events[self.incommingCoin ](data)
         return
+    def __getIfTubeIsFull(self, decimal_number):
+        # Convierte el entero decimal en una cadena binaria de 8 bits
+        binary_string = bin(decimal_number)[2:].zfill(8)
+        for i in range(len(binary_string)):
+            if binary_string[i] == "1":
+                self.tubeFullState.append(1)
+            else:
+                self.tubeFullState.append(0)
 
-    """--------------------------fullTube------------------------------"""
-    def __fullTube(self,tube):
-        print("FULL TUBE Number " + str(tube))
-    """--------------------------cashBack------------------------------"""
+        # Muestra los estados de las alarmas
+        print(self.tubeFullState)
+
     def cashBack(self,moneyBack):
-        """
-            CashBack(R) Esta función sirve
-            para devolver el dinero indicado por parámetros
-        """
-        print("CoinWallet.cashback:  DEVOLVER" + str(moneyBack))
+        print("Coin return : " + str(moneyBack))
         # MONEDERO OLD
         # countCoins = moneyBack / 0.05
         countCoins = moneyBack / 0.01
@@ -162,41 +152,16 @@ class CoinWalletController(SerialCommunicator):
             countCoins = countCoins - 255
             time.sleep(5)
         return self.__sendCommand([0x0F, 0x02, int(countCoins) ])
-    """-------------------------- PUBLIC FUNCTIONS ------------------------------"""
-    """-------------------------- startThreadReceived ------------------------------"""
-    """-------------------------- SETUP ------------------------------"""
     def setup(self): 
         return self.__sendCommand(SETUP)
     
-    """-------------------------- Reset ------------------------------"""
     def reset(self): 
         return self.__sendCommand(RESET)
     
     def enableDisableChargeCoins(self): 
         return self.__sendCommandAndReceive([0x0C,0xFF,0xFF])
     
-    """-------------------------- tubeStatus ------------------------------"""
     def tubeStatus(self):  
-        """
-        VMC Command Code Changer Response Data
-        TUBE STATUS 0AH 18 bytes: Z1 - Z18
-        Z1 - Z2 = Tube Full Status - 2 bytes
-        Indicates status of coin tube for coin types 0 to 15.
-        b15 b14 b13 b12 b11 b10 b9 b8 | b7 b6 b5 b4 b3 b2 b1 b0
-         Z1 Z2
-        A bit is set to indicate a full tube. For example, bit 7 = set would
-        indicate the tube for coin type 7 is full.
-        Z3 - Z18 = Tube Status - 16 bytes
-        Indicates the greatest number of coins that the changer “knows”
-        definitely are present in the coin tubes. A bytes position in the
-        16 byte string indicates the number of coins in a tube for a 
-        Multi-Drop Bus / Internal Communication Protocol
-        MDB/ICP Version 4.3 July, 2019 5•6
-        particular coin type. For example, the first byte sent indicates
-        the number of coins in a tube for coin type 0. Unsent bytes are
-        assumed to be zero. For tube counts greater than 255, counts
-        should remain at 255. 
-        """
         try:
             self.com.readline()
             time.sleep(.3)
@@ -205,50 +170,48 @@ class CoinWalletController(SerialCommunicator):
             response = self.com.readline().split()
             int_array = [int(x, 16) for x in response]
             
-            print("lengtth ",len(response))
+            # print("lengtth ",len(response))
             if len(response)!= 19 :
                 AssertionError("Error reading tube status")
 
-            print("TUBE STATUS: ",response)
+            # print("TUBE STATUS: ",response)
+            self.getTubesCount(int_array)
             
-            tubeByte1 = int_array[0]
-            tubeByte2 = int_array[1]
-
-            self.checkIfTubeIsFull(tubeByte1,tubeByte2)
-
-            self.tubeQnty_0_05 = int_array[2]
-            self.tubeQnty_0_10 = int_array[3]
-            self.tubeQnty_0_20 = int_array[4]
-            self.tubeQnty_0_50 = int_array[5]
-            self.tubeQnty_1_00 = int_array[6]
-            self.tubeQnty_2_00 = int_array[7]
-
-            print(" tubeQnty_0_05:  ",self.tubeQnty_0_05)
-            print(" tubeQnty_0_10:  ",self.tubeQnty_0_10)
-            print(" tubeQnty_0_20:  ",self.tubeQnty_0_20)
-            print(" tubeQnty_0_50:  ",self.tubeQnty_0_50)
-            print(" tubeQnty_1_00:  ",self.tubeQnty_1_00)
-            print(" tubeQnty_2_00:  ",self.tubeQnty_2_00)
-
-            self.availableMoneyInCoins = 0
-            self.availableMoneyInCoins += self.tubeQnty_0_05 * 0.05
-            self.availableMoneyInCoins += self.tubeQnty_0_10 * 0.10
-            self.availableMoneyInCoins += self.tubeQnty_0_20 * 0.20
-            self.availableMoneyInCoins += self.tubeQnty_0_50 * 0.50
-            self.availableMoneyInCoins += self.tubeQnty_1_00 
-            self.availableMoneyInCoins += self.tubeQnty_2_00 * 2
-            self.availableMoneyInCoins = round(self.availableMoneyInCoins,2)
-            
-            print("availableMoneyInCoins: ",self.availableMoneyInCoins)
+            self.countAvailableMoney()
             return 
         except Exception as e:
             print(e)
             self.tubeStatus()
             pass
-
+    def getTubesCount(self,arrayResponse):
         
-    """-------------------------- poll ------------------------------"""
-    """ NOT IN USE SUST BY THREAD """
+        tubeByte1 = arrayResponse[0]
+        tubeByte2 = arrayResponse[1]
+        self.checkIfTubeIsFull(tubeByte1,tubeByte2)
+
+        self.tubeQnty_0_05 = arrayResponse[2]
+        self.tubeQnty_0_10 = arrayResponse[3]
+        self.tubeQnty_0_20 = arrayResponse[4]
+        self.tubeQnty_0_50 = arrayResponse[5]
+        self.tubeQnty_1_00 = arrayResponse[6]
+        self.tubeQnty_2_00 = arrayResponse[7]
+
+        print(" tubeQnty_0_05:  ",self.tubeQnty_0_05)
+        print(" tubeQnty_0_10:  ",self.tubeQnty_0_10)
+        print(" tubeQnty_0_20:  ",self.tubeQnty_0_20)
+        print(" tubeQnty_0_50:  ",self.tubeQnty_0_50)
+        print(" tubeQnty_1_00:  ",self.tubeQnty_1_00)
+        print(" tubeQnty_2_00:  ",self.tubeQnty_2_00)
+    def countAvailableMoney(self):
+        self.availableMoneyInCoins = 0
+        self.availableMoneyInCoins += self.tubeQnty_0_05 * 0.05
+        self.availableMoneyInCoins += self.tubeQnty_0_10 * 0.10
+        self.availableMoneyInCoins += self.tubeQnty_0_20 * 0.20
+        self.availableMoneyInCoins += self.tubeQnty_0_50 * 0.50
+        self.availableMoneyInCoins += self.tubeQnty_1_00 
+        self.availableMoneyInCoins += self.tubeQnty_2_00 * 2
+        self.availableMoneyInCoins = round(self.availableMoneyInCoins,2)
+        print("availableMoneyInCoins: ",self.availableMoneyInCoins)
     def poll(self):  # 0x0B
         interval = 0.2
         while True:
@@ -260,80 +223,18 @@ class CoinWalletController(SerialCommunicator):
             if wait > 0.0:
                 time.sleep(wait)
         
-    """-------------------------- EnableInsertCoins ------------------------------"""
     def coinType(self):  # 0x0C
-        """
-        Y1 - Y2 = Coin Enable - 2 bytes
-        b15 b14 b13 b12 b11 b10 b9 b8 | b7 b6 b5 b4 b3 b2 b1 b0
-        Y1 Y2
-        A bit is set to indicate a coin type is accepted. For example, bit 6 is set to
-        indicate coin type 6, bit 15 is set to indicate coin type 15, and so on. To
-        disable the changer, disable all coin types by sending a data block containing
-        0000H. All coins are automatically disabled upon reset.
-        Y3 - Y4 = Manual Dispense Enable - 2 bytes
-        b15 b14 b13 b12 b11 b10 b9 b8 | b7 b6 b5 b4 b3 b2 b1 b0
-        Y3 Y4
-        A bit is set to indicate dispense enable. For example, bit 2 is set to enable
-        dispensing of coin type 2. This command enables/disables manual
-        dispensing using optional inventory switches. All manual dispensing switches
-        are automatically enabled upon reset. 
-        """
         return self.__sendCommand([0x0C])
     
-    """-------------------------- Dispense ------------------------------"""
-    """  NOT IN USE """
-    def dispense(self): # 0D
-        return self.__sendCommand([0x0D,0x05])
-    
-    """-------------------------- EnableInsertCoins ------------------------------"""
     def enableInsertCoins(self):
-        """
-            This function enable the possibility
-            of insert all kind of coins
-        """
         print("enableInsert Coins")
         return self.__sendCommand([0x0C, 0xff, 0xff, 0xff, 0xff])
-    """-------------------------- EnableInsertCoins ------------------------------"""
+    
     def disableInsertCoins(self):
-        """
-            This function disable the possibility
-            of insert all kind of coins
-        """
         print("disableInsert Coins")
         return self.__sendCommandAndReceive([0x0C, 0x00, 0x00, 0x00, 0x00])
-
-    """--------------------------cashBack------------------------------"""
-    def cashBackRoutine(self,moneyBack):
-        """
-            CashBack(R) Esta función sirve
-            para devolver el dinero indicado por parámetros
-            realizando la rutina completa
-        """
-        self.reset()
-        time.sleep(0.5)
-        self.setup()
-        time.sleep(0.5)
-        self.enableInsertCoins()
-        time.sleep(0.5)
-        self.tubeStatus()
-        time.sleep(0.5)
-        return self.cashBack(round(moneyBack,2))
-    """--------------------------startReceivingMode------------------------------"""
-    def startReceivingMode(self):
-        """
-            This function start the service
-        """
-        #self.proc = multiprocessing.Process(target= self.threadReceived, args=())
-        self.enableInsertCoins()
-
-#         self.proc.daemon = True
-#         self.proc.start()
-#         self.enableInsertCoins()
-        return 0
- 
-    """-------------------------- startThreadReceived ------------------------------"""
+    
     async def threadReceived(self):
-#         while not self.statusDeactiveThread:
         while True:
             if(self.enableReceivedMode):
                 if(self.com.in_waiting):    
@@ -356,26 +257,12 @@ class CoinWalletController(SerialCommunicator):
                     self.data = str(self.data)
                     self.status = str(self.status)
     
-    def getIfTubeIsFull(self, decimal_number):
-        # Convierte el entero decimal en una cadena binaria de 8 bits
-        binary_string = bin(decimal_number)[2:].zfill(8)
-        for i in range(len(binary_string)):
-            if binary_string[i] == "1":
-                self.tubeFullState.append(1)
-                
-            else:
-                self.tubeFullState.append(0)
-
-        # Muestra los estados de las alarmas
-        print(self.tubeFullState)
-    
     def checkIfTubeIsFull(self,byte1,byte2):
         self.tubeFullState = []
-        self.getIfTubeIsFull(byte1)
-        self.getIfTubeIsFull(byte2)
+        self.__getIfTubeIsFull(byte1)
+        self.__getIfTubeIsFull(byte2)
         for i in range(len(self.tubeFullState)):
             if self.tubeFullState[i] == 1:
                 print ("FULL TUBE number: ", i)
                 #TODO: REVISAR
                 # self.sendErrorTPV("ERROR: El tubo numero " + str(i) + " se encuentra lleno.")
-
